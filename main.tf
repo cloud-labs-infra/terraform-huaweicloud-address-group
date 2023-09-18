1f767c858e5123c82e7399b7dbcf59a2dc1df6fe
Aleksandr Titov <t.sander.v@gmail.com>
2023-09-18 14:03:55 +0300

    fix: Fix naming

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name = var.name_postfix == null ? format("%s-key", var.name) : format("%s-key-%s", var.name, var.name_postfix)
+  name = var.name_postfix == null ? format("%s-address-group", var.name) : format("%s-address-group-%s", var.name, var.name_postfix)
 }
 
 resource "huaweicloud_vpc_address_group" "main" {
